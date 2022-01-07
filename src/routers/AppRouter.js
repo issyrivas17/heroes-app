5caadbb7ebee2a6687a4f22545c245bd58d42cf4
issyrivas17 <ismeniarivas17@gmail.com>
2022-01-07 10:21:22 -0300

    Se soluciono el error que tenia en la app, se debia instalar npm install >> save react router dom por eso no corria la aplicacion

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,23 @@
 import React from 'react' 
-import {Routes,Route, BrowserRouter} from "react-router-dom";
+import {Routes,Route,BrowserRouter} from "react-router-dom";
 import { DcScreen } from '../components/Dc/DcScreen';
 import { LoginScreen } from '../components/Login/LoginScreen';
 import { MarvelScreen } from '../components/Marvel/MarvelScreen';
 import { SearchScreen } from '../components/search/SearchScreen';
+import { Navbar } from '../components/ui/Navbar';
   
   
 
 export const AppRouter = () => {
     return (
         < BrowserRouter>
-            <h1>Welcome to React router!</h1>
+          <Navbar/>
            <Routes> 
               <Route path = "/" element={<MarvelScreen />} /> 
               <Route path = "/marvel" element = {<MarvelScreen/>} /> 
               <Route path = "/dc" element = {<DcScreen/>} /> 
               <Route path = "/search" element = {<SearchScreen/>} /> 
               <Route path = "/login" element = {<LoginScreen/>} /> 
-
-
-
                </Routes> 
         </ BrowserRouter>
     )
