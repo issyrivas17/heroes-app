7719e60e3f52d1bc93c55e893def4d979670fd16
issyrivas17 <ismeniarivas17@gmail.com>
2022-01-07 10:36:55 -0300

    Se cambio el navlink por un button se aplico un console.log todo funciona bien

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
+ 
+    const handleLogout = () => {
+        console.log('logout') 
+    }
+
+
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             
@@ -38,9 +45,8 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 <ul className="navbar-nav ml-auto">
                     <button
-                        
                         className="nav-item nav-link btn" 
-                       
+                        onClick={handleLogout}
                     >
                         Logout
                     </button>
