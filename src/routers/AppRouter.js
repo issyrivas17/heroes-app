3455b308fc31ef59e79d78737937ba05b7330541
issyrivas17 <ismeniarivas17@gmail.com>
2022-01-12 13:51:45 -0300

    Se creo el segundo router el loginScreen lleva a una pagina que no tiene navbar

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,23 +1,17 @@
 import React from 'react' 
 import {Routes,Route,BrowserRouter} from "react-router-dom";
-import { DcScreen } from '../components/Dc/DcScreen';
-import { LoginScreen } from '../components/Login/LoginScreen';
-import { MarvelScreen } from '../components/Marvel/MarvelScreen';
-import { SearchScreen } from '../components/search/SearchScreen';
-import { Navbar } from '../components/ui/Navbar';
+import { LoginScreen } from '../components/Login/LoginScreen'; 
+import { DashboardRoutes } from './DashboardRoutes';
   
   
 
 export const AppRouter = () => {
     return (
-        < BrowserRouter>
-          <Navbar/>
+        < BrowserRouter> 
            <Routes> 
-              <Route path = "/" element={<MarvelScreen />} /> 
-              <Route path = "/marvel" element = {<MarvelScreen/>} /> 
-              <Route path = "/dc" element = {<DcScreen/>} /> 
-              <Route path = "/search" element = {<SearchScreen/>} /> 
               <Route path = "/login" element = {<LoginScreen/>} /> 
+
+              <Route path="/*" element= {<DashboardRoutes/>} /> 
                </Routes> 
         </ BrowserRouter>
     )
--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -1,9 +1,22 @@
-import { Navbar } from "../components/ui/Navbar"
+import {Routes,Route} from "react-router-dom";
+
+import { Navbar } from "../components/ui/Navbar" 
+import { DcScreen } from '../components/Dc/DcScreen';
+
+import { MarvelScreen } from '../components/Marvel/MarvelScreen';
+import { SearchScreen } from '../components/search/SearchScreen';
 
 export const DashboardRoutes = () => {
     return (
         <>
             <Navbar/> 
+            <Routes> 
+              <Route path = "/" element={<MarvelScreen />} /> 
+              <Route path = "/dc" element = {<DcScreen/>} /> 
+              <Route path = "/search" element = {<SearchScreen/>} /> 
+              <Route path = "/marvel" element = {<MarvelScreen/>} />
+               </Routes> 
         </>
     )
 }
+
