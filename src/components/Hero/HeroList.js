1eac7c0b4a9a8442ec31cc5f52dc0ae88a5c2e3c
issyrivas17 <ismeniarivas17@gmail.com>
2022-01-18 00:01:23 -0300

    Se creo un getHeroByid

---
+++ src/components/Hero/HeroList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { getHeroesByPublisher } from '../../selectors/getHeroById'
+
+export const HeroList = ({publisher}) => {
+
+    const heroes = getHeroesByPublisher(publisher); 
+    return (
+        <>
+            <h1>Hero List- {publisher} </h1> 
+
+            <ul>
+                {
+                    heroes.map(hero=>(
+                        <li key = {hero.id}>
+                            {hero.superhero} 
+                        </li> 
+                    ))
+                }
+             
+            </ul>
+        </>
+    )
+}
