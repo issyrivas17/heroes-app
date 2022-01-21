155b1616e290e88ea7e80aee9ec26fccf7162116
issyrivas17 <ismeniarivas17@gmail.com>
2022-01-21 12:34:35 -0300

    Se aplica el useParams pero cuando destructuro el arreglo el console log me toma undefined.

--- src/components/Hero/HeroScreen.js
+++ src/components/Hero/HeroScreen.js
@@ -3,8 +3,8 @@ import { useParams } from 'react-router-dom'
 
 export const HeroScreen = () => {
 
-    const{heroeId}= useParams(); 
-    console.log(heroeId)
+    const {heroeId}= useParams(); 
+    console.log(heroeId); 
     
     return (
         <>
