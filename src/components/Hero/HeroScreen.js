a132310a4006636af9ccdae26cd3446b80228639
issyrivas17 <ismeniarivas17@gmail.com>
2022-01-20 22:22:35 -0300

    Se paso el userParams en una consts destructurada pero el valor es undefined, hay que revisar.

--- src/components/Hero/HeroScreen.js
+++ src/components/Hero/HeroScreen.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export const HeroScreen = () => {
+
+    const{heroeId}= useParams(); 
+    console.log(heroeId)
+    
     return (
         <>
-            <h1>HeroScreen</h1>
+            <h1>HeroScreen</h1> 
+
+            
         </>
     )
 }
