7d67da735956e990b0dc6a8f8e599bd09fc2bc89
issyrivas17 <ismeniarivas17@gmail.com>
2022-01-21 14:34:01 -0300

    Se esta agregando estilos y nuevas constantes al heroScreen, pero no me carga la imagen debo revisa consta imagePath

--- src/components/Hero/HeroScreen.js
+++ src/components/Hero/HeroScreen.js
@@ -11,8 +11,22 @@ export const HeroScreen = () => {
     if (!hero) {
         return <Navigate to = '/' /> 
     } 
+   
+    const {
+        id,
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters
+    } = hero ; 
 
-   const imagePath = `/assest/heroes/${hero.id}.jpg`;
+
+   const imagePath=`/assest/${id}.jpg`; 
+
+   const handleReturn = () => {
+       
+   }
 
 
 
@@ -26,7 +40,22 @@ export const HeroScreen = () => {
         
         />   
            </div>   
-          <d 
+          <div className='col-8'> 
+          <h3>{hero.superhero}</h3> 
+          <ul className= 'list-group list-group'> 
+          <li className='list-group-item'> <b>Alter ego:</b> {alter_ego}</li>
+          <li className='list-group-item'> <b>Publisher:</b> {publisher}</li>
+          <li className='list-group-item'> <b>First appearance:</b> {first_appearance}</li>
+          </ul> 
+         <h5>Characters</h5> 
+         <p>{characters}</p> 
+
+         <button className=' btn btn-outline-info' 
+         onClick= {handleReturn} 
+    
+         > Return </button>
+          
+          </div>
         </div>
     )
 }
