7897c2747b76921039f1622450630ece8e9a6142
issyrivas17 <ismeniarivas17@gmail.com>
2022-01-24 23:57:27 -0300

    Se agrego un BackInDown en la HeroList, posterior se trabajara con las tarjetas de superHeroes al interno.

--- src/components/Hero/HeroList.js
+++ src/components/Hero/HeroList.js
@@ -9,7 +9,7 @@ export const HeroList = ({publisher}) => {
     const heroes = useMemo(() => getHeroesByPublisher(publisher),[publisher]);   
      
     return (
-        <div className='row rows-cols-3 row-cols-md-3 g-3'>
+        <div className='row rows-cols-3 row-cols-md-3 g-3 animate__animated animate__backInDown'>
 
                 {
                     heroes.map(hero=>(
