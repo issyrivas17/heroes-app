4b6be0496143e4c9d4973a38096d352ebcdab7da
issyrivas17 <ismeniarivas17@gmail.com>
2022-01-24 23:43:23 -0300

    El useMemo no se esta renderizando se debe ver bien que pasa

--- src/components/Hero/HeroList.js
+++ src/components/Hero/HeroList.js
@@ -1,10 +1,13 @@
+import { useMemo } from 'react';
+
 import React from 'react'
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
 import { HeroCards } from './HeroCards';
 
 export const HeroList = ({publisher}) => {
 
-    const heroes = getHeroesByPublisher(publisher); 
+    const heroes = useMemo(() => getHeroesByPublisher(publisher),[publisher]);   
+     
     return (
         <div className='row rows-cols-3 row-cols-md-3 g-3'>
 
