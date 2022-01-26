00566e284b56da10fa9f1aca5d2c6fc88d5e4b6d
issyrivas17 <ismeniarivas17@gmail.com>
2022-01-26 20:25:36 -0300

    Se termino de trabajar con el animate.css

--- src/components/Hero/HeroScreen.js
+++ src/components/Hero/HeroScreen.js
@@ -36,11 +36,11 @@ export const HeroScreen = () => {
         <img 
         src={imagePath} 
          alt ={superhero} 
-         className='img-thumbnail animate__animated animate__fadeInLeft' 
+         className='img-thumbnail animate__animated animate__fadeInLeft'
         
         />   
            </div>   
-          <div className='col-8'> 
+          <div className='col-8 animate__animated animate__fadeInLeft'> 
           <h3>{hero.superhero}</h3> 
           <ul className= 'list-group list-group'> 
           <li className='list-group-item'> <b>Alter ego:</b> {alter_ego}</li>
