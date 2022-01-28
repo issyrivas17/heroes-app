d955ac7b8c0c41b34462d67d9520eb166a02c491
issyrivas17 <ismeniarivas17@gmail.com>
2022-01-28 14:12:11 -0300

    se instalo query-string para utilizarlos en las constante

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,4 +1,6 @@
 import React from 'react' 
+import queryString from 'query-string';
+
 import { useNavigate,useLocation } from 'react-router-dom';
 import { getHeroesByName } from '../../selectors/getHeroesByName';
 import { HeroCards } from '../Hero/HeroCards';
@@ -13,7 +15,8 @@ export const SearchScreen = () => {
 
    const location = useLocation();  
 
-   console.log(location); 
+  const query = queryString.parse(location.search);
+  console.log(query)
 
 
     const [formValues,handleInputChange] = useForm ({
