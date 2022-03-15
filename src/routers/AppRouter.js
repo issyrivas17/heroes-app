de38f6a19df3c59605c69a666b5b24ffd541362f
issyrivas17 <ismeniarivas17@gmail.com>
2022-03-15 17:16:10 -0300

    Se hace la ruta publica, si estas autenticado no deberia de redireccionarte a login hay un error que si permite llegar al login

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,6 +3,7 @@ import {Routes,Route,BrowserRouter} from "react-router-dom";
 import { LoginScreen } from '../components/Login/LoginScreen'; 
 import { DashboardRoutes } from './DashboardRoutes';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
   
   
 
@@ -10,8 +11,19 @@ export const AppRouter = () => {
     return (
         < BrowserRouter>  
            <Routes> 
-              <Route path = "/login" element = {<LoginScreen/>} /> 
+              {/* <Route path = "/login" element = {<LoginScreen/>} />  */} 
+
+              <Route path='/login' element={ 
+                 <PublicRoute> 
+                     <LoginScreen/> 
+                 </PublicRoute>  
+              } 
+              /> 
   
+
+
+
+
              <Route path='/*' element={
                  <PrivateRoute> 
                     <DashboardRoutes /> 
--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom';
+import { useContext } from 'react/cjs/react.development';
+import { AuthContext } from '../auth/authContext';
 
-export const PublicRoute = () => {
-  return (
-    <div>PublicRoute</div>
-  )
-}
+export const PublicRoute = ({children}) => { 
+
+    const {user} = useContext (AuthContext); 
+
+  return user.logged
+  ? <Navigate to= "/marvel" />  
+  :children 
+} 
