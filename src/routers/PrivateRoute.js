922175fe9d15b6f01f972e273c872c5e4a108af4
issyrivas17 <ismeniarivas17@gmail.com>
2022-03-15 16:53:00 -0300

    Se arreglo el error estaba importando algo que no debia estar

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom';
 import { useContext } from 'react/cjs/react.development';
-import { Children } from 'react/cjs/react.production.min';
 
 import { AuthContext } from '../auth/authContext';
 
@@ -11,6 +10,6 @@ export const PrivateRoute = ({children}) => {
  const {user} = useContext(AuthContext);  
 
  return user.logged 
- ? Children 
+ ? children 
  : <Navigate to = "/login" />  
 }
